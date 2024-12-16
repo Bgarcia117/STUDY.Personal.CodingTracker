52bd6c691e79514a2c42694d74e5430aed224d8d
Bgarcia117 <77687480+Bgarcia117@users.noreply.github.com>
2024-12-16 10:12:47 -0600

    Added additional message
    Testing out Github features

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello there!");
 Console.WriteLine("General Kenobi!");
+
+Console.WriteLine("Additonal Changes");
