02fe7b85dd7af0fef1ae9db0f9407c462e945d43
Bgarcia117 <bryant.garcia.1771@gmail.com>
2024-12-16 10:48:13 -0600

    Initial Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,16 @@ Console.WriteLine("Hello there!");
 Console.WriteLine("General Kenobi!");
 
 Console.WriteLine("Additonal Changes");
+
+
+void MyFirstMethod() {
+    // Do Something
+}
+
+void MySecondMethod() {
+    // Do Something
+}
+
+void MyThirdMethod() {
+    // Do Something
+}
